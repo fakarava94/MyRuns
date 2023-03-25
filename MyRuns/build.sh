a33a8628ef48f469fb213b6cb717de3e2a6d97e8
fakarava94 <francois.libespere@gmail.com>
2023-03-25 23:50:53 +0100

    debug

--- MyRuns/build.sh
+++ MyRuns/build.sh
@@ -4,6 +4,7 @@ set -o errexit  # exit on error
 
 pip install --upgrade pip
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
+echo "static ..."
+ls -1 MyRuns/static
 python manage.py migrate
