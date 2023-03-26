55bb1210c35d82f5812dac0b8962d0612b4b1322
fakarava94 <francois.libespere@gmail.com>
2023-03-26 22:13:31 +0200

    debug

--- MyRuns/MyRuns/celery.py
+++ MyRuns/MyRuns/celery.py
@@ -9,14 +9,20 @@ app = Celery('MyRuns',
              broker=os.getenv("CELERY_BROKER_URL"),
              backend='redis://red-cgetk8pmbg568r47cnl0:6379')
 
+print ('>>> Celery app=",app)
+
+
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
+print ('>>> app.config_from_object done")
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
+print ('>>> app.autodiscover_tasks done")
+
 
 @app.task(bind=True)
 def debug_task(self):
