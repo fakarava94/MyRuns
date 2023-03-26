c5e531781dd6f78c35835b124a80067e18ce3d47
fakarava94 <francois.libespere@gmail.com>
2023-03-26 21:36:49 +0200

    debug

--- MyRuns/MyRuns/wsgi.py
+++ MyRuns/MyRuns/wsgi.py
@@ -14,8 +14,10 @@ from django.contrib.staticfiles.handlers import StaticFilesHandler
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MyRuns.settings')
-if settings.DEBUG:
-    application = StaticFilesHandler(get_wsgi_application())
-else:
-    application = get_wsgi_application()
+application = get_wsgi_application()
+
+#if settings.DEBUG:
+#    application = StaticFilesHandler(get_wsgi_application())
+#else:
+#    application = get_wsgi_application()
 
