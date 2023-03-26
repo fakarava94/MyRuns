11fced4ccfdc4b0f422a526a7d815a2e6166c78e
fakarava94 <francois.libespere@gmail.com>
2023-03-26 14:59:44 +0200

    debug

--- MyRuns/build.sh
+++ MyRuns/build.sh
@@ -7,4 +7,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 echo "static ..."
 ls -1 /opt/render/project/src/MyRuns/static
+ln -s /opt/render/project/src/MyRuns/static /opt/render/project/src/static
 python manage.py migrate
