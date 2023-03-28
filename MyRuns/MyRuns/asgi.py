4ce2c604778e5cae4ef56357419edb175abcb8ba
fakarava94 <francois.libespere@gmail.com>
2023-03-28 23:16:52 +0200

    Debug

--- MyRuns/MyRuns/asgi.py
+++ MyRuns/MyRuns/asgi.py
@@ -1,4 +1,9 @@
 import os
-from channels.asgi import get_channel_layer
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MyRuns.settings")
-channel_layer = get_channel_layer()
+import django
+from channels.routing import get_default_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proj.settings")
+
+django.setup()
+
+application = get_default_application()
