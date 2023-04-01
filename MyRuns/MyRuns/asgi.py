31655a4f6bebe5650e7e034eab1c68ed54cbbe9d
fakarava94 <francois.libespere@gmail.com>
2023-04-01 14:37:20 +0200

    Debug

--- MyRuns/MyRuns/asgi.py
+++ MyRuns/MyRuns/asgi.py
@@ -1,9 +1,4 @@
 import os
-import django
-from channels.routing import get_default_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proj.settings")
-
-django.setup()
-
-application = get_default_application()
+from channels.asgi import get_channel_layer
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MyRuns.settings")
+channel_layer = get_channel_layer()
--- MyRuns/build.sh
+++ MyRuns/build.sh
@@ -2,10 +2,8 @@
 
 set -o errexit  # exit on error
 
+uname -a
 pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-echo "static ..."
-ls -1 /opt/render/project/src/MyRuns/static
-ln -s /opt/render/project/src/MyRuns/static /opt/render/project/src/static
 python manage.py migrate
