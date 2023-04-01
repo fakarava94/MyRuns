26261f0407fd60d5949f4e991fd2734d26043135
fakarava94 <francois.libespere@gmail.com>
2023-04-01 14:46:11 +0200

    Debug

--- MyRuns/websocketSrv.py
+++ MyRuns/websocketSrv.py
@@ -1,17 +1,34 @@
 import asyncio
+import http
+import signal
 
 import websockets
 
-# create handler for each connection
 
-async def handler(websocket, path):
+async def echo(websocket):
+    async for message in websocket:
+        await websocket.send(message)
 
-    data = await websocket.recv()
-    reply = f"Data recieved as:  {data}!"
 
-    await websocket.send(reply)
+async def health_check(path, request_headers):
+    if path == "/healthz":
+        return http.HTTPStatus.OK, [], b"OK\n"
 
-start_server = websockets.serve(handler, "localhost", 3000)
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+async def main():
+    # Set the stop condition when receiving SIGTERM.
+    loop = asyncio.get_running_loop()
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+
+    async with websockets.serve(
+        echo,
+        host="",
+        port=8080,
+        process_request=health_check,
+    ):
+        await stop
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
