9087a143c29883ebbae92c92ea80db8c378e9410
fakarava94 <francois.libespere@gmail.com>
2023-04-10 22:23:22 +0200

    Debug

--- MyRuns/MyRuns/routing.py
+++ MyRuns/MyRuns/routing.py
@@ -4,16 +4,12 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from strava2.consumers import Consumers
-from django.core.asgi import get_asgi_application
-
-django_asgi_app = get_asgi_application()
  
 application = ProtocolTypeRouter({
 
     # Channels will do this for you automatically. It's included here as an example.
     # "http": AsgiHandler,
     # Django's ASGI application to handle traditional HTTP requests
-    "http": django_asgi_app,
 
     # Route all WebSocket requests to our custom chat handler.
     # We actually don't need the URLRouter here, but we've put it in for
