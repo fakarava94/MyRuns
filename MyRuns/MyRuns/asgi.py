7eb4267c4913969082067dcbc2a77226bc2b0290
fakarava94 <francois.libespere@gmail.com>
2023-04-10 18:27:03 +0200

    Debug

--- MyRuns/MyRuns/asgi.py
+++ MyRuns/MyRuns/asgi.py
@@ -1,5 +1,7 @@
 import os
 #from channels.asgi import get_channel_layer
 from channels.layers import get_channel_layer
+from django.core.asgi import get_asgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MyRuns.settings")
 channel_layer = get_channel_layer()
+application = get_asgi_application()
