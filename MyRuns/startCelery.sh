904cddfc751eec846debad66c8e30c1c394701bd
fakarava94 <francois.libespere@gmail.com>
2023-04-20 22:24:28 +0200

    Debug

--- MyRuns/startCelery.sh
+++ MyRuns/startCelery.sh
@@ -1,4 +1,4 @@
-celery multi start w1 w2 --app MyRuns --concurrency=4 --loglevel=info --logfile=./celery.log --pidfile=$PWD/%n.pid
+celery multi start w1 w2 --app MyRuns --concurrency=4 --loglevel=info --logfile=./celery.log
 ./monitorWorkers.sh &
 tail -F ./celery.log &
 gunicorn app:app
