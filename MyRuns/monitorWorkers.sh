72364d2221f4a4e530df7e65476257f60826cc31
fakarava94 <francois.libespere@gmail.com>
2023-04-21 13:04:13 +0200

    Debug

--- MyRuns/monitorWorkers.sh
+++ MyRuns/monitorWorkers.sh
@@ -2,11 +2,11 @@ while sleep 15; do
    #status=`celery status -A MyRuns`
    status=`celery status`
    echo $status
-   if echo $status | egrep "worker@.*: OK" > /dev/null 2>&1
+   if echo $status | egrep "celery@.*: OK" > /dev/null 2>&1
    then
-      echo "w1 Online"
+      echo "worker Online"
    else
-      echo "w1 Offline"
+      echo "worker Offline"
       #celery multi restart w1 -A MyRuns  --concurrency=2 --loglevel=info --logfile=celery.log
    fi
    #if echo $status | egrep "w2@.*: OK" > /dev/null 2>&1
--- MyRuns/startCelery.sh
+++ MyRuns/startCelery.sh
@@ -1,6 +1,6 @@
 #celery multi start w1 -A MyRuns -c 4 -f $PWD/celery.log -p $PWD/%n.pid
 #celery multi start w2 -A MyRuns -c 4 -f $PWD/celery.log -p $PWD/%n.pid
-celery --app MyRuns  worker --loglevel=DEBUG --concurrency=2 --beat  & 
+celery --app MyRuns  worker --loglevel=DEBUG --concurrency=2 -E --detach
 ./monitorWorkers.sh &
 # -F ./celery.log &
 gunicorn app:app
