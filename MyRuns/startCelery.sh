ad31769bce4bc1173537ae2c418765d93b47d709
fakarava94 <francois.libespere@gmail.com>
2023-04-22 00:44:49 +0200

    Debug

--- MyRuns/startCelery.sh
+++ MyRuns/startCelery.sh
@@ -1,8 +1,8 @@
 #celery multi start w1 -A MyRuns -c 4 -f $PWD/celery.log -p $PWD/%n.pid
 #celery multi start w2 -A MyRuns -c 4 -f $PWD/celery.log -p $PWD/%n.pid
 
-# celery beat --detach
-# celery --app MyRuns  worker --loglevel=DEBUG --concurrency=2 -E &
+celery beat --detach
+celery --app MyRuns  worker --loglevel=DEBUG --concurrency=2 -E &
 # ./monitorWorkers.sh &
 # tail -F ./celery.log &
 gunicorn app:app --timeout 300
