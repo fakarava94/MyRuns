8db38f110e84cad5bacbe80f1c657e9a72ac2132
fakarava94 <francois.libespere@gmail.com>
2023-04-21 12:26:03 +0200

    Debug

--- MyRuns/monitorWorkers.sh
+++ MyRuns/monitorWorkers.sh
@@ -2,18 +2,18 @@ while sleep 15; do
    #status=`celery status -A MyRuns`
    status=`celery status`
    echo $status
-   if echo $status | egrep "w1@.*: OK" > /dev/null 2>&1
+   if echo $status | egrep "worker@.*: OK" > /dev/null 2>&1
    then
       echo "w1 Online"
    else
       echo "w1 Offline"
-      celery multi restart w1 -A MyRuns  --concurrency=2 --loglevel=info --logfile=celery.log
+      #celery multi restart w1 -A MyRuns  --concurrency=2 --loglevel=info --logfile=celery.log
    fi
-   if echo $status | egrep "w2@.*: OK" > /dev/null 2>&1
-   then
-      echo "w2 Online"
-   else
-      echo "w2 Offline"
-      celery multi restart w2 -A MyRuns  --concurrency=2 --loglevel=info --logfile=celery.log
-   fi
+   #if echo $status | egrep "w2@.*: OK" > /dev/null 2>&1
+   #then
+   #   echo "w2 Online"
+   #else
+   #   echo "w2 Offline"
+   #   celery multi restart w2 -A MyRuns  --concurrency=2 --loglevel=info --logfile=celery.log
+   #fi
 done
--- MyRuns/startCelery.sh
+++ MyRuns/startCelery.sh
@@ -1,5 +1,6 @@
-celery multi start w1 -A MyRuns -c 4 -f $PWD/celery.log -p $PWD/%n.pid
-celery multi start w2 -A MyRuns -c 4 -f $PWD/celery.log -p $PWD/%n.pid
+#celery multi start w1 -A MyRuns -c 4 -f $PWD/celery.log -p $PWD/%n.pid
+#celery multi start w2 -A MyRuns -c 4 -f $PWD/celery.log -p $PWD/%n.pid
+celery --app MyRuns  worker --loglevel=DEBUG --concurrency=2 --beat  & 
 ./monitorWorkers.sh &
-tail -F ./celery.log &
+# -F ./celery.log &
 gunicorn app:app
