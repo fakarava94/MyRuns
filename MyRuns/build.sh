e78704503846d6f17b2bac2b00956d6b464131eb
fakarava94 <francois.libespere@gmail.com>
2023-06-17 18:25:57 +0200

    Debug

--- MyRuns/build.sh
+++ MyRuns/build.sh
@@ -8,3 +8,10 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations strava2
 python manage.py migrate
+
+python manage.py shell << FF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+User.objects.create_superuser('admin', 'email', 'password')
+exit()
+FF
