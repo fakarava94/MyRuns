4b2c1d6f45a243e58754c34b44a1d334f0cd4654
fakarava94 <francois.libespere@gmail.com>
2023-06-19 00:16:59 +0200

    Debug

--- MyRuns/build.sh
+++ MyRuns/build.sh
@@ -7,7 +7,9 @@ pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
-python manage.py migrate
+python manage.py migrate 
+python manage.py makemigrations strava2
+python manage.py migrate  strava2
 
 #python manage.py shell << FF
 #from django.contrib.auth import get_user_model
