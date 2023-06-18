b8d1ba00a002a61bef22a65f0587dc53228c6269
fakarava94 <francois.libespere@gmail.com>
2023-06-19 00:45:26 +0200

    Debug

--- MyRuns/build.sh
+++ MyRuns/build.sh
@@ -6,7 +6,7 @@ uname -a
 pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py makemigrations
+python manage.py makemigrations strava2
 python manage.py migrate 
 
 #python manage.py shell << FF
