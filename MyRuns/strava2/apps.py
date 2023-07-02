9367ed33f07487d5fa8c8e8e94e0a00f20ca02c7
fakarava94 <francois.libespere@gmail.com>
2023-07-02 14:54:21 +0200

    Debug

--- MyRuns/strava2/apps.py
+++ MyRuns/strava2/apps.py
@@ -24,8 +24,13 @@ class Strava2Config(AppConfig):
                 token['expires_at'] = user.token_expires_at
                 refresh_token = getRefreshedToken(login[0].clientID, login[0].clientSecret, token)
 
+                print('refresh_token=', refresh_token)
+
                 client = Client(refresh_token['access_token'])
                 login=Login.objects.filter(id=1)
-                subscribeUrl = re.sub('callback', 'subscribeCB',  login[0].callbackURL)  
+
+                subscribeUrl = re.sub('callback', 'subscribeCB',  login[0].callbackURL)
+                print('subscribeUrl=', subscribeUrl)
                 log.info ('subscribeUrl=%s',subscribeUrl)
+                
                 client.create_subscription(login[0].clientID, login[0].clientSecret, subscribeUrl, verify_token=u'STRAVA')
